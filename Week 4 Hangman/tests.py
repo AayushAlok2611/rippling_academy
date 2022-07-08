94f2a4593ae0f3c88d6017dd95a57545b1489239
Aayush Alok <aayushalok8b@gmail.com>
2022-07-08 20:20:53 +0530

    Added tests for GuessCheck class

--- Week 4 Hangman/tests.py
+++ Week 4 Hangman/tests.py
@@ -1,8 +1,7 @@
 import unittest
-from main import PrintString
+from main import PrintString,GuessCheck
 
 class TestPrintString(unittest.TestCase):
-    
     def test_initialPrint(self):
         printString = PrintString()
         self.assertEqual( printString.initialPrint("") , [] , "The dashed string is not of same length as input word" )
@@ -12,4 +11,27 @@ class TestPrintString(unittest.TestCase):
         printString = PrintString()
         self.assertEqual( printString.correctGuess( ['a','b'] , "abcd" ) , ['a','b','_','_'] )
 
+class TestGuessCheck(unittest.TestCase):
+    def test_alreadyGuessedChar(self):
+        guessCheck = GuessCheck()
+        
+        guessCheck.correctGuesses.append('a')
+        guessCheck.correctGuesses.append('b')
+        guessCheck.incorrectGuesses.append('c')
+        guessCheck.incorrectGuesses.append('d')
+
+        self.assertEqual(guessCheck.alreadyGuessedChar('e'),False)
+        self.assertEqual(guessCheck.alreadyGuessedChar('a'),True)
+        
+    def test_isCorrect(self):
+        guessCheck = GuessCheck()
+
+        self.assertEqual(guessCheck.isCorrect('e','abcd') , False)
+        self.assertEqual(guessCheck.isCorrect('a','abcd') , True)
+
+    def test_wordGuessed(self):
+        guessCheck = GuessCheck()
+
+        self.assertEqual(guessCheck.wordGuessed(["a","b"] ,"ab" ) , True)
+        self.assertEqual(guessCheck.wordGuessed(["a","b"] ,"ac" ) , False)
 
