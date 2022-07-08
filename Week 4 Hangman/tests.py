25a7c66f61f35781a5fc1c51cb5159085aa81d0b
Aayush Alok <aayushalok8b@gmail.com>
2022-07-08 20:28:08 +0530

    Fixed error

--- Week 4 Hangman/tests.py
+++ Week 4 Hangman/tests.py
@@ -5,7 +5,7 @@ class TestPrintString(unittest.TestCase):
     def test_initialPrint(self):
         printString = PrintString()
         self.assertEqual( printString.initialPrint("") , [] , "The dashed string is not of same length as input word" )
-        self.assertEqual( printString.initialPrint("abcd") , ['_','_','_','_'] , "The dashed string is not of same length as input word" )
+        self.assertEqual( printString.initialPrint("abcd") , ['_ ','_ ','_ ','_ '] , "The dashed string is not of same length as input word" )
     
     def test_correctGuess(self):
         printString = PrintString()
