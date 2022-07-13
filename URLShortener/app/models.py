f6f37827a3b6f113d6c4a9a813318fe9b487925f
Aayush Alok <aayushalok8b@gmail.com>
2022-07-13 22:08:11 +0530

    Removed UserModel which was used tio test DB connection

--- URLShortener/app/models.py
+++ URLShortener/app/models.py
@@ -3,6 +3,3 @@ from django.db import models
 from mongoengine import *
 
 # Create your models here.
-class UserModel(Document):
-    name = StringField(max_length=30,required=True)
-    password = StringField(max_length=30,required=True)
