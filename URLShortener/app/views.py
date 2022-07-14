18e8c9f64a7e21e36a69e368dbb25a94d51f6fb9
Aayush Alok <aayushalok8b@gmail.com>
2022-07-14 12:32:53 +0530

    Added basic signup user view

--- URLShortener/app/views.py
+++ URLShortener/app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from app.models import UserModel
 from django. views. decorators. csrf import csrf_exempt
+from app.models import User,ShortURL
 
 # Create your views here.
 
@@ -11,4 +11,16 @@ def landing(request):
 
 
 #testing if adding to db works
-
+@csrf_exempt
+def signupUser(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username,password)
+        if len(User.objects(username=username)) !=  0:
+            return HttpResponse('User already exists')
+        user = User(username = username,password = password)
+        user.save()
+        s = str(username) + "  " + str(password)
+        return HttpResponse(s)
+    return HttpResponse("GET Request -> A form will be rendered to sign up user")
