269f8dc4b9336c3719885b16313928d28025ce6b
Aayush Alok <aayushalok8b@gmail.com>
2022-07-14 14:13:40 +0530

    Added basic login route and view

--- URLShortener/URLShortener/urls.py
+++ URLShortener/URLShortener/urls.py
@@ -20,5 +20,6 @@ from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.landing),
-    path('signup',views.signupUser)
+    path('signup',views.signupUser),
+    path('login',views.loginUser)
 ]
--- URLShortener/app/views.py
+++ URLShortener/app/views.py
@@ -5,22 +5,34 @@ from app.models import User,ShortURL
 
 # Create your views here.
 
-#landing page -> first tthing visible to an unauthenticated user
 def landing(request):
     return HttpResponse("You can either login here or sign up")
 
-
-#testing if adding to db works
 @csrf_exempt
 def signupUser(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-        print(username,password)
-        if len(User.objects(username=username)) !=  0:
+        existingUsers = User.objects(username=username)
+        if len( existingUsers ) >  0:
             return HttpResponse('User already exists')
         user = User(username = username,password = password)
         user.save()
+        print(user.id)
         s = str(username) + "  " + str(password)
         return HttpResponse(s)
     return HttpResponse("GET Request -> A form will be rendered to sign up user")
+
+@csrf_exempt
+def loginUser(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        existingUsers = User.objects(username=username)
+
+        #No existing users or existing user but incorrect password
+        if len(existingUsers) ==  0 or existingUsers[0].password != password:
+            return HttpResponse('Username or password incorrect')
+        return HttpResponse('You are logged in')
+        
+    return HttpResponse('GET Request -> A form will be rendered to login user')
