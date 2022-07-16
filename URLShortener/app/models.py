62ad6d6eb2d5e22e402bae7a2ce98cafb306cdd4
Aayush Alok <aayushalok8b@gmail.com>
2022-07-16 11:37:57 +0530

    Made changes to User and ShortURL models

--- URLShortener/app/models.py
+++ URLShortener/app/models.py
@@ -1,7 +1,7 @@
 from telnetlib import DO
 from django.db import models
 from mongoengine import *
-
+from users.models import User
 
 # Create your models here.
 class ShortURL(Document):
@@ -9,4 +9,6 @@ class ShortURL(Document):
     originalURL = StringField(max_length=None)
     hitCount = IntField()
     lastHit = DateTimeField()
+    user = ReferenceField(User,reverse_delete_rule=CASCADE)
+
 
--- URLShortener/users/models.py
+++ URLShortener/users/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 from mongoengine import *
 import mongoengine
-from app.models import ShortURL
 
 # Create your models here.
 class User(Document):
     username = StringField()
     password = StringField()
-    shortURLs = ListField(ReferenceField(ShortURL,reverse_delete_rule = mongoengine.PULL))
 
